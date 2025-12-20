5267ef05baea36b6efc604a9b7553f46fc6dfd47
Gaurav Kapur <shivammittal2124@gmail.com>
2025-12-20 23:02:21 +0530

    fixed errors

--- apps/orchestrator/src/index.ts
+++ apps/orchestrator/src/index.ts
@@ -1,1 +1,26 @@
-console.log("hello world");
+import { Worker } from "bullmq";
+import { QUEUE_NAMES } from "@repo/queue";
+import {Redis} from "ioredis";
+
+const connection = new Redis({
+    host: "localhost",
+    port: 6379,
+    maxRetriesPerRequest: null
+})
+const worker = new Worker(QUEUE_NAMES.PROMPT_QUEUE , 
+    async(job) => {
+        console.log(job.data);
+    },
+    {
+        connection: connection
+    }
+);
+
+worker.on('completed' , (job,result) => {
+    console.log('Job completed:', job.id);
+    console.log('Result:', result);
+});
+
+worker.on('closed', () => {
+    console.log('Worker closed');
+  });
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,6 +1,22 @@
-import { PrismaClient } from "../generated/prisma/index.js";
+import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
-export const prisma = new PrismaClient();
+const { Pool } = pg;
 
-export * from "../generated/prisma/index.js";
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+const pool = new Pool({
+  connectionString: databaseUrl,
+});
+
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({ adapter });
+
+export * from "../generated/prisma/client.js";
 export { PrismaClient };
